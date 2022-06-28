6154caf5117bbc6693ecc16ed485aed27c0b7b9a
Mark Stacey <markjstacey@gmail.com>
2022-06-28 09:55:06 -0230

    Add validation to production build script
    Validation has been added to the build script when the "prod" target is
    selected. We now ensure that all expected environment variables are
    set, and that no extra environment variables are present (which might
    indicate that the wrong configuration file is being used).
    
    The `prod` target uses a new `.metamaskprodrc` configuration file. Each
    required variable can be specified either via environment variable or
    via this config file. CI will continue set these via environment
    variable, but for local manual builds we can use the config file to
    simplify the build process and ensure consistency.
    
    A new "dist" target has been added to preserve the ability to build a
    "production-like" build without this validation.
    
    The config validation is invoked early in the script, in the CLI
    argument parsing step, so that it would fail more quickly. Otherwise
    we'd have to wait a few minutes longer for the validation to run.
    This required some refactoring, moving functions to the utility module
    and moving the config to a dedicated module.
    
    Additionally, support has been added for all environment variables to
    be set via the config file. Previously the values `PUBNUB_PUB_KEY`,
    `PUBNUB_SUB_KEY`, `SENTRY_DSN`, and `SWAPS_USE_DEV_APIS` could only be
    set via environment variable. Now, all of these variables can be set
    either way.
    
    Closes #15003

--- development/generate-lavamoat-policies.sh
+++ development/generate-lavamoat-policies.sh
@@ -7,6 +7,6 @@ set -o pipefail
 # Generate LavaMoat policies for the extension background script for each build
 # type.
 # ATTN: This may tax your device when running it locally.
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only &&
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta &&
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask
+WRITE_AUTO_POLICY=1 yarn build scripts:dist --policy-only &&
+WRITE_AUTO_POLICY=1 yarn build scripts:dist --policy-only --build-type beta &&
+WRITE_AUTO_POLICY=1 yarn build scripts:dist --policy-only --build-type flask

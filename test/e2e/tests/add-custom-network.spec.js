71567d515505b2782774673537b204ecd197fbd7
mirjanaKukic <mirjana.kukic@endava.com>
2022-08-08 15:13:37 +0200

    lint fix

--- test/e2e/tests/add-custom-network.spec.js
+++ test/e2e/tests/add-custom-network.spec.js
@@ -24,41 +24,76 @@ describe('Custom network', function () {
         await driver.press('#password', driver.Key.ENTER);
 
         await driver.clickElement('.account-menu__icon');
-        await driver.clickElement({ tag: 'div', text: 'Settings'});
+        await driver.clickElement({ tag: 'div', text: 'Settings' });
 
-        await driver.clickElement({ tag: 'div' , text: 'Experimental'});
+        await driver.clickElement({ tag: 'div', text: 'Experimental' });
         const toggleList = await driver.findElements('.toggle-button');
         toggleList[2].click();
 
         await driver.clickElement('.network-display');
-        await driver.clickElement({ tag: 'button', text: 'Add Network'});
+        await driver.clickElement({ tag: 'button', text: 'Add Network' });
 
-        const addButtons = await driver.findElements({ tag: 'button', text: 'Add'});
+        const addButtons = await driver.findElements({
+          tag: 'button',
+          text: 'Add',
+        });
         addButtons[0].click();
 
-        const title = await driver.findElement({tag: 'span', text: 'Arbitrum One'});
-        assert.equal(await title.getText(), 'Arbitrum One', 'Title of popup should be selected network');
+        const title = await driver.findElement({
+          tag: 'span',
+          text: 'Arbitrum One',
+        });
+        assert.equal(
+          await title.getText(),
+          'Arbitrum One',
+          'Title of popup should be selected network',
+        );
 
         const networkLabels = await driver.findElements('dd');
-        assert.equal(networkLabels.length, 4, 'There should be 4 labels : Network Name, Network URL, Chain ID and Currency Symbol');
+        assert.equal(
+          networkLabels.length,
+          4,
+          'There should be 4 labels : Network Name, Network URL, Chain ID and Currency Symbol',
+        );
 
-        assert.equal(await networkLabels[2].getText(), '42161', 'Chain ID should be 42161');
-        assert.equal(await networkLabels[3].getText(), 'AETH', 'Currency symbol should be AETH');
+        assert.equal(
+          await networkLabels[2].getText(),
+          '42161',
+          'Chain ID should be 42161',
+        );
+        assert.equal(
+          await networkLabels[3].getText(),
+          'AETH',
+          'Currency symbol should be AETH',
+        );
 
-        await driver.clickElement({ tag: 'a', text: 'View all details'});
+        await driver.clickElement({ tag: 'a', text: 'View all details' });
 
-        const networkDetailsLabels = await driver.findElements('dd'); 
-        assert.equal(await networkDetailsLabels[8].getText(), 'https://explorer.arbitrum.io', 'Block Explorer URL is not correct');
+        const networkDetailsLabels = await driver.findElements('dd');
+        assert.equal(
+          await networkDetailsLabels[8].getText(),
+          'https://explorer.arbitrum.io',
+          'Block Explorer URL is not correct',
+        );
 
-        await driver.clickElement({ tag: 'button', text: 'Close'});
-        await driver.clickElement({ tag: 'button', text: 'Approve'});
+        await driver.clickElement({ tag: 'button', text: 'Close' });
+        await driver.clickElement({ tag: 'button', text: 'Approve' });
 
-        await driver.clickElement({ tag: 'h6', text: 'Switch to Arbitrum One'});
+        await driver.clickElement({
+          tag: 'h6',
+          text: 'Switch to Arbitrum One',
+        });
 
-        const networkName = await driver.findElement({tag: 'span', text: 'Arbitrum One'});
-        assert.equal(await networkName.getText(), 'Arbitrum One', 'You have not switched to Arbitrum Network');
-
-    },
+        const networkName = await driver.findElement({
+          tag: 'span',
+          text: 'Arbitrum One',
+        });
+        assert.equal(
+          await networkName.getText(),
+          'Arbitrum One',
+          'You have not switched to Arbitrum Network',
+        );
+      },
     );
   });
 });
